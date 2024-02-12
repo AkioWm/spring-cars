e051f617cb9c6a08c8efacc5873145c6178868be
AkioWm <cherif.kante16@gmail.com>
2024-02-12 15:53:53 +0100

    Add CustomProperties

---
+++ src/main/java/com/garage/cars/CustomProperties.java
@@ -0,0 +1,15 @@
+package com.garage.cars;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "com.garage.cars")
+public class CustomProperties {
+    private String apiUrl;
+
+
+}
