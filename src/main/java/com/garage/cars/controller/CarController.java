dd3e5ce72535d69916728b8dd186035b3be754aa
AkioWm <cherif.kante16@gmail.com>
2024-02-14 18:37:24 +0100

    Add get and delete car feature

--- src/main/java/com/garage/cars/controller/CarController.java
+++ src/main/java/com/garage/cars/controller/CarController.java
@@ -1,15 +1,30 @@
 package com.garage.cars.controller;
 
+import com.garage.cars.model.Car;
+import com.garage.cars.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CarController {
-
+    @Autowired
+    private CarService carService;
     @GetMapping("/")
-    String home(){
+    String home(Model model){
+        Iterable<Car> carList = carService.getCars();
+        model.addAttribute("cars",carList);
         return "home";
     }
-
+    @GetMapping("/deleteCar/{id}")
+    public ModelAndView deleteCar(@PathVariable("id") int id){
+    carService.deleteCar(id);
+    return new ModelAndView("redirect:/");
+    }
 
 }
